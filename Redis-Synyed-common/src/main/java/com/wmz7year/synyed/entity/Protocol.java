db6768fa34ea45797af1ea269b24df1ffeb88d58
wmz7year <ydswcy513@gmail.com>
2015-12-21 21:48:38 +0800

    添加Redis同步管道实体类

--- Redis-Synyed-common/src/main/java/com/wmz7year/synyed/entity/Protocol.java
+++ Redis-Synyed-common/src/main/java/com/wmz7year/synyed/entity/Protocol.java
@@ -12,5 +12,84 @@ package com.wmz7year.synyed.entity;
  * @version V1.0
  */
 public class Protocol {
+	/**
+	 * 源redis
+	 */
+	private RedisServer srcRedis;
+	/**
+	 * 目标redis
+	 */
+	private RedisServer descRedis;
+
+	public Protocol() {
+		super();
+	}
+
+	public Protocol(RedisServer srcRedis, RedisServer descRedis) {
+		super();
+		this.srcRedis = srcRedis;
+		this.descRedis = descRedis;
+	}
+
+	public RedisServer getSrcRedis() {
+		return srcRedis;
+	}
+
+	public void setSrcRedis(RedisServer srcRedis) {
+		this.srcRedis = srcRedis;
+	}
+
+	public RedisServer getDescRedis() {
+		return descRedis;
+	}
+
+	public void setDescRedis(RedisServer descRedis) {
+		this.descRedis = descRedis;
+	}
+
+	/*
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descRedis == null) ? 0 : descRedis.hashCode());
+		result = prime * result + ((srcRedis == null) ? 0 : srcRedis.hashCode());
+		return result;
+	}
+
+	/*
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Protocol other = (Protocol) obj;
+		if (descRedis == null) {
+			if (other.descRedis != null)
+				return false;
+		} else if (!descRedis.equals(other.descRedis))
+			return false;
+		if (srcRedis == null) {
+			if (other.srcRedis != null)
+				return false;
+		} else if (!srcRedis.equals(other.srcRedis))
+			return false;
+		return true;
+	}
+
+	/*
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Protocol [srcRedis=" + srcRedis + ", descRedis=" + descRedis + "]";
+	}
 
 }
