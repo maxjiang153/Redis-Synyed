9e11414c94723bc2fd8ef0241c314a818d4f91b1
wmz7year <ydswcy513@gmail.com>
2015-12-21 21:36:20 +0800

    去掉redis数据包对象的序列化支持

--- Redis-Synyed-common/src/main/java/com/wmz7year/synyed/packet/redis/RedisPacket.java
+++ Redis-Synyed-common/src/main/java/com/wmz7year/synyed/packet/redis/RedisPacket.java
@@ -1,10 +1,5 @@
 package com.wmz7year.synyed.packet.redis;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
-
 /**
  * Redis数据包<br>
  * 该数据包为最原始的对象
@@ -15,7 +10,7 @@ import java.io.ObjectOutput;
  * @date 2015年12月10日 下午3:11:43
  * @version V1.0
  */
-public abstract class RedisPacket implements Externalizable {
+public abstract class RedisPacket {
 	/**
 	 * redis命令
 	 */
@@ -32,23 +27,4 @@ public abstract class RedisPacket implements Externalizable {
 	public void setCommand(String command) {
 		this.command = command;
 	}
-
-	/*
-	 * @see java.io.Externalizable#writeExternal(java.io.ObjectOutput)
-	 */
-	@Override
-	public void writeExternal(ObjectOutput out) throws IOException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * @see java.io.Externalizable#readExternal(java.io.ObjectInput)
-	 */
-	@Override
-	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-
-	}
-
 }
