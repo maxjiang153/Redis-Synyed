c597b98a842ffe0934efd1f854041f08d6d21db4
wmz7year <ydswcy513@gmail.com>
2015-12-21 17:25:38 +0800

    重构redis命令转换工具类

--- Redis-Synyed-common/src/main/java/com/wmz7year/synyed/parser/entry/RedisRDBCommand.java
+++ Redis-Synyed-common/src/main/java/com/wmz7year/synyed/parser/entry/RedisRDBCommand.java
@@ -41,41 +41,100 @@ public class RedisRDBCommand {
 	 */
 	public List<String> getCommands() {
 		List<String> commands = new ArrayList<String>();
-		if (value instanceof RedisStringObject) { // string 类型的value 生成redis命令
-													// set key value
-			StringBuilder builder = new StringBuilder();
-			builder.append(SET).append(' ').append(key.toCommand()).append(' ').append(value.toCommand());
-			commands.add(builder.toString());
-		} else if (value instanceof RedisZipListObject) { // list类型的value
-															// 生成redis lpush key
-															// value value..
-			StringBuilder builder = new StringBuilder();
-			builder.append(LPUSH).append(' ').append(key.toCommand()).append(' ').append(value.toCommand());
-			commands.add(builder.toString());
+		if (value instanceof RedisHashObject) {
+			// TODO
+			System.out.println("RedisHashObject");
 		} else if (value instanceof RedisHashZipList) {
-			RedisHashZipList hashZipList = (RedisHashZipList) value;
-			if (hashZipList.getElementCount() % 2 == 0) { // hset类型数据 生成命令 hset
-															// key flag value
-				List<String> elements = hashZipList.getElements();
-				for (int i = 0; i < hashZipList.getElementCount(); i += 2) {
-					StringBuilder builder = new StringBuilder();
-					builder.append(HSET).append(' ').append(key.toCommand()).append(' ').append(elements.get(i))
-							.append(' ').append(elements.get(i + 1));
-					commands.add(builder.toString());
-				}
-			} else {
-				System.out.println("RedisHashZipList value：" + value);
-			}
-		} else if (value instanceof RedisZSetZipList) { //  zadd key field value field value field value
-			StringBuilder builder = new StringBuilder();
-			builder.append(ZADD).append(' ').append(key.toCommand()).append(' ').append(value.toCommand());
-			commands.add(builder.toString());
+			// hash set类型数据 生成hset命令
+			commands.addAll(createHSETCommand());
+		} else if (value instanceof RedisHashZipMap) {
+			// TODO
+			System.out.println("RedisHashZipMap");
+		} else if (value instanceof RedisListObject) {
+			// TODO
+			System.out.println("RedisListObject");
+		} else if (value instanceof RedisSetIntSet) {
+			// TODO
+			System.out.println("RedisSetIntSet");
+		} else if (value instanceof RedisSetObject) {
+			// TODO
+			System.out.println("RedisSetObject");
+		} else if (value instanceof RedisStringObject) {
+			// string 类型的value 生成redis set命令
+			commands.add(createSETCommand());
+		} else if (value instanceof RedisZipListObject) {
+			// list类型的value 生成lpush命令
+			commands.add(createLPUSHCommand());
+		} else if (value instanceof RedisZSetObject) {
+			// TODO
+			System.out.println("RedisZSetObject");
+		} else if (value instanceof RedisZSetZipList) {
+			// sorted set类型数据 生成zadd命令
+			commands.add(createZADDCommand());
 		} else {
-			System.out.println("value：" + value);
-
+			throw new IllegalStateException("不支持的数据类型：" + value.getClass().getName());
 		}
 		// TODO 过期时间
 		return commands;
+	}
+
+	/**
+	 * 根据key value 创建set命令的方法<br>
+	 * 
+	 * set key value
+	 * 
+	 * @return set命令
+	 */
+	private String createSETCommand() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(SET).append(' ').append(key.toCommand()).append(' ').append(value.toCommand());
+		return builder.toString();
+	}
+
+	/**
+	 * 根据key value创建lpush命令的方法<br>
+	 * 
+	 * lpush key value value..
+	 * 
+	 * @return lpush命令
+	 */
+	private String createLPUSHCommand() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(LPUSH).append(' ').append(key.toCommand()).append(' ').append(value.toCommand());
+		return builder.toString();
+	}
+
+	/**
+	 * 根据key value创建hset命令<br>
+	 * 
+	 * hset key flag value
+	 * 
+	 * @return hset命令
+	 */
+	private List<String> createHSETCommand() {
+		List<String> commands = new ArrayList<String>();
+		RedisHashZipList hashZipList = (RedisHashZipList) value;
+		List<String> elements = hashZipList.getElements();
+		for (int i = 0; i < hashZipList.getElementCount(); i += 2) {
+			StringBuilder builder = new StringBuilder();
+			builder.append(HSET).append(' ').append(key.toCommand()).append(' ').append(elements.get(i)).append(' ')
+					.append(elements.get(i + 1));
+			commands.add(builder.toString());
+		}
+		return commands;
+	}
+
+	/**
+	 * 根据key value创建zadd命令<br>
+	 * 
+	 * zadd key field value
+	 * 
+	 * @return zadd命令
+	 */
+	private String createZADDCommand() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(ZADD).append(' ').append(key.toCommand()).append(' ').append(value.toCommand());
+		return builder.toString();
 	}
 
 	/*
